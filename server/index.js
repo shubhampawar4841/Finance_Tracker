335748bfd4f1d1c258fd732fce15181e2f6b2358
shubhampawar4841 <shubhampawar4036@gmail.com>
2025-02-17 19:50:43 +0530

    vercel

--- server/index.js
+++ server/index.js
@@ -23,3 +23,6 @@ app.get("/", (req, res) => {
 // ✅ Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
+module.exports = app;
+
